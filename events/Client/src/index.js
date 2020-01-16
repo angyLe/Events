180f7f1394c28fdbb3ef0d159f945fbcffdde01d
Angy <indidel@mail.ru>
2020-01-16 20:54:52 +0100

    Install redux, react-redux and eslint-plugin-react-redux.
    eslint plugin for redux enforcing best practices for react-redux.

--- events/Client/src/index.js
+++ events/Client/src/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import "./index.css";
+import App from "./App";
+import combineReducers from "./combineReducers";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const configureStore = initialState => {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const middlewares = [thunk];
+  const enhancer = composeEnhancers(
+    applyMiddleware(...middlewares)
+    // other store enhancers if any
+  );
+
+  const store = createStore(combineReducers, initialState, enhancer);
+  return store;
+};
+
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
