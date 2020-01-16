68f7a6afb89d938938005235d452416563cf5373
Angy <indidel@mail.ru>
2020-01-16 18:08:02 +0100

    Add project files.

---
+++ events/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using events.Models;
+
+namespace events.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/home")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+        [HttpGet]
+        [Route("Test")]
+        public IActionResult Test()
+        {
+
+            int bla = 1;
+            return Json(bla);
+        }
+        
+
+       /* [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        */
+    }
+}
