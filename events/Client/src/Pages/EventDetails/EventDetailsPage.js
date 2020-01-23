d599849057a480c938d70575243f345eef429bdc
Angy <indidel@mail.ru>
2020-01-23 19:23:43 +0100

    Add link to all events from detail page.

--- events/Client/src/Pages/EventDetails/EventDetailsPage.js
+++ events/Client/src/Pages/EventDetails/EventDetailsPage.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import EventDetails from "../../Components/Events/EventDetails";
+import tr from "../../Utils/translationHelper";
 import "./EventDetailsPage.css";
 
 // eslint-disable-next-line react/prefer-stateless-function
@@ -7,7 +9,10 @@ export default class EventDetailsPage extends Component {
   render() {
     return (
       <div className="Event-Details-Wrapper">
-        <div>All events</div>
+        <div className="Event-Details-Back-All-Events">
+          <Link to="/">{tr("AllEvents", "All events")}</Link>
+        </div>
+        <br />
         <EventDetails />
       </div>
     );
