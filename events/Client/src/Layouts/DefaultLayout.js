edaf346c18ed9cb8f150c8703eeef11f9c470c0b
Angy <indidel@mail.ru>
2020-01-23 19:23:00 +0100

    Add LanguagePicker to DefaultLayout

--- events/Client/src/Layouts/DefaultLayout.js
+++ events/Client/src/Layouts/DefaultLayout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import AppLogo from "../UI/Logo"
+import AppLogo from "../UI/Logo";
+import LanguagePicker from "../Components/Languages/LanguagePicker";
 import "./DefaultLayout.css";
 import { DEFAULT_PADDINGS_CLASS_NAME } from "../constants";
 
 const DefaultLayout = props => {
-  const { children } = props;
+  const { children, languages } = props;
   return (
     <div className="App">
       <header className={`App-header ${DEFAULT_PADDINGS_CLASS_NAME}`}>
@@ -13,7 +14,7 @@ const DefaultLayout = props => {
           <AppLogo />
           City events
         </div>
-        <p className="App-menu">Menu</p>
+        <LanguagePicker />
       </header>
       <div className={`App-main ${DEFAULT_PADDINGS_CLASS_NAME}`}>
         {children}
