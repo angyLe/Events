7d24e2884c8bfd91ed448dfe88ffcdf2c5691e01
Angy <indidel@mail.ru>
2020-01-23 19:28:05 +0100

    Move PageNotFound to separate file.

---
+++ events/Client/src/Pages/PageNotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+import tr from "../Utils/translationHelper";
+
+const PageNotFound = () => {
+  return (
+    <div className="pageNotFound">{tr("PageNotFound", "Page not Found")}</div>
+  );
+};
+
+export default PageNotFound;
