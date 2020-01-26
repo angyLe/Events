d04a745f1a866d41fe2e76e3c3309a4274685840
Angy <indidel@mail.ru>
2020-01-26 20:33:17 +0100

    Changed Semantic ui modal to be a controlled component.
    Open/Close state will be controlled outside of component.

--- events/Client/src/UI/Modal.js
+++ events/Client/src/UI/Modal.js
@@ -3,10 +3,17 @@ import PropTypes from "prop-types";
 import { Button, Modal } from "semantic-ui-react";
 
 const ModalWindow = props => {
-  const { triggerComponent, header, scrolling, children } = props;
+  const {
+    triggerComponent,
+    header,
+    scrolling,
+    open,
+    children,
+    onClose
+  } = props;
 
   return (
-    <Modal closeIcon trigger={triggerComponent}>
+    <Modal onClose={onClose} open={open} closeIcon trigger={triggerComponent}>
       <Modal.Header>{header}</Modal.Header>
       <Modal.Content scrolling={scrolling}>{children}</Modal.Content>
     </Modal>
@@ -14,16 +21,20 @@ const ModalWindow = props => {
 };
 
 ModalWindow.defaultProps = {
+  open: false,
   triggerComponent: <Button>Open</Button>,
   header: "Provide header",
-  scrolling: true
+  scrolling: true,
+  onClose: () => {}
 };
 
 ModalWindow.propTypes = {
+  open: PropTypes.bool,
   triggerComponent: PropTypes.node,
   header: PropTypes.string,
   children: PropTypes.node.isRequired,
-  scrolling: PropTypes.bool
+  scrolling: PropTypes.bool,
+  onClose: PropTypes.func
 };
 
 export default ModalWindow;
