7b046ff18bf0cdfc0c8c5a1690cc1fb2db0a7545
Angy <indidel@mail.ru>
2020-01-26 20:28:46 +0100

    Add constants.
    Add constants for semantic ui flags and icon names.

--- events/Client/src/constants.js
+++ events/Client/src/constants.js
@@ -4,3 +4,9 @@ export const DEFAULT_PADDINGS_CLASS_NAME = "Default-paddings";
 export const VALUTA = "NOK";
 
 export const DEFAULT_LANG = "en";
+
+/* See list of flag names: https://react.semantic-ui.com/elements/flag/ */
+export const SEMANTIC_UI_FLAGS = { en: "america", nb: "norway" };
+
+/* Icons from https://react.semantic-ui.com. They uses font awesome icons */
+export const ICON_NAMES = { pencil: "pencil", add: "add" };
