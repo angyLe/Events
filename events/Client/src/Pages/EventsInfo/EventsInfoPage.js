24c31931bbdda051ab9183dee5ff21cb42050e26
Angy <indidel@mail.ru>
2020-01-29 12:52:19 +0100

    Connected EventsInfoPage to redux state.

--- events/Client/src/Pages/EventsInfo/EventsInfoPage.js
+++ events/Client/src/Pages/EventsInfo/EventsInfoPage.js
@@ -1,20 +1,66 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 import EventsList from "../../Features/Events/EventsList";
 import EventsDatePickerPanel from "../../Features/Events/EventDatepickerPanel";
+import {
+  fetchEventsFromServer,
+  selectEvents
+} from "../../Features/Events/EventsHandlers";
 import "./EventsInfoPage.css";
 
 // eslint-disable-next-line react/prefer-stateless-function
-export default class EventsInfoPage extends Component {
-  render() {
-    return (
-      <div className="Events-Info-Wrapper">
-        <div>
-          <EventsDatePickerPanel />
-        </div>
-        <div>
-          <EventsList eventsList={null} />
-        </div>
+export const EventsInfoPage = props => {
+  const { eventsList } = props;
+  const { fetchEvents } = props;
+
+  console.log("eventsList");
+  console.log(eventsList);
+
+  useEffect(() => {
+    console.log("effect");
+
+    if (!eventsList) {
+      fetchEvents();
+    }
+  });
+
+  return (
+    <div className="Events-Info-Wrapper">
+      <div>
+        <EventsDatePickerPanel />
       </div>
-    );
-  }
-}
+      <div>
+        <EventsList eventsList={eventsList} />
+      </div>
+    </div>
+  );
+};
+
+EventsInfoPage.defaultProps = {
+  eventsList: null
+};
+
+EventsInfoPage.propTypes = {
+  eventsList: PropTypes.instanceOf(Array),
+  fetchEvents: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    eventsList: selectEvents(state)
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchEvents: () => {
+      dispatch(fetchEventsFromServer());
+    }
+  };
+};
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(EventsInfoPage)
+);
