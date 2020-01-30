6d8e285d1bc0830484dc84c5a854681d7c46c594
Angy <indidel@mail.ru>
2020-01-30 11:23:37 +0100

    Connected Event to redux store directly. Created selector to retrive data about event.
    This way is recomended by Redux developers.

--- events/Client/src/Features/Events/Event.js
+++ events/Client/src/Features/Events/Event.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { createSelector } from "@reduxjs/toolkit";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Events.css";
 import { Card } from "semantic-ui-react";
 import dayjs from "dayjs";
 import EventPrice from "./EventPrice";
 import "./Event.css";
+import { selectEventTranslationById } from "./EventTranslationHandlers";
+import { selectEventById } from "./EventsHandlers";
+
+export const selectEventWithTranslation = createSelector(
+  [selectEventTranslationById, selectEventById],
+  (eventTranslation, event) => {
+    return { ...eventTranslation, ...event };
+  }
+);
 
 const Event = props => {
-  const { imgSrc, title, shortDescription, price, dateTime } = props;
+  const { event } = props;
+
+  const { imgSrc, title, shortDescription, price, dateTimeFrom } = event;
 
   return (
     <div className="Event-element">
@@ -20,7 +34,7 @@ const Event = props => {
         className="Event-element-card"
         image={imgSrc || ""}
         header={title || ""}
-        meta={dayjs(dateTime).format("DD MMMM, HH:MM")}
+        meta={dayjs(dateTimeFrom).format("DD MMMM, HH:MM")}
         description={shortDescription || ""}
         extra={
           <div>
@@ -33,16 +47,26 @@ const Event = props => {
 };
 
 Event.defaultProps = {
-  imgSrc:
-    "https://cdn.pixabay.com/photo/2018/05/31/12/02/celebration-3443810_960_720.jpg"
+  event: {
+    imgSrc:
+      "https://cdn.pixabay.com/photo/2018/05/31/12/02/celebration-3443810_960_720.jpg"
+  }
 };
 
 Event.propTypes = {
-  imgSrc: PropTypes.string,
-  title: PropTypes.string.isRequired,
-  shortDescription: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-  dateTime: PropTypes.string.isRequired
+  event: PropTypes.shape({
+    imgSrc: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    shortDescription: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    dateTimeFrom: PropTypes.string.isRequired
+  })
 };
 
-export default Event;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    event: selectEventWithTranslation(state, ownProps)
+  };
+};
+
+export default withRouter(connect(mapStateToProps)(Event));
