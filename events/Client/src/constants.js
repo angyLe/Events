57aa1976df3a313a7dddee0fff458ab9dcdf878c
Angy <indidel@mail.ru>
2020-02-04 19:53:30 +0100

    Add russian flag

--- events/Client/src/constants.js
+++ events/Client/src/constants.js
@@ -6,7 +6,7 @@ export const VALUTA = "NOK";
 export const DEFAULT_LANG = "en";
 
 /* See list of flag names: https://react.semantic-ui.com/elements/flag/ */
-export const SEMANTIC_UI_FLAGS = { en: "america", nb: "norway" };
+export const SEMANTIC_UI_FLAGS = { en: "america", nb: "norway", ru: "russia"};
 
 /* Icons from https://react.semantic-ui.com. They uses font awesome icons */
 export const ICON_NAMES = { pencil: "pencil", add: "add" };
