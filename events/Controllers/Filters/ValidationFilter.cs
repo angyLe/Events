d4c624e52b7d5c724a338927b214c5b157ed8cf4
Angy <indidel@mail.ru>
2020-02-05 21:16:12 +0100

    Capitalize of namespace name.

--- events/Controllers/Filters/ValidationFilter.cs
+++ events/Controllers/Filters/ValidationFilter.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System.Collections.Generic;
 
-namespace events.Controllers.Filters
+namespace Events.Controllers.Filters
 {
     public class ValidationFilter : Attribute, IActionFilter
     {
