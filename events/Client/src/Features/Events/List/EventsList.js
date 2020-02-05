255d835c840f406ba5e16d65d206d2c88efb8295
Angy <indidel@mail.ru>
2020-02-05 11:07:19 +0100

    Use object key as component key.
    Since key may be event or event translation id.

--- events/Client/src/Features/Events/List/EventsList.js
+++ events/Client/src/Features/Events/List/EventsList.js
@@ -16,9 +16,10 @@ export default class EventsList extends Component {
     if (!objIsEmpty(eventsList)) {
       eventsListResult = Object.keys(eventsListObj).map(el => {
         const element = eventsListObj[el];
+ 
         return (
           <Event
-            key={element.id}
+            key={el}
             currentLangId={currentLangId}
             eventId={element.eventId}
             navigateToEvent={() =>
@@ -42,50 +43,7 @@ export default class EventsList extends Component {
 }
 
 EventsList.defaultProps = {
-  eventsList: [
-    {
-      id: 1,
-      imgSrc:
-        "https://p2.piqsels.com/preview/386/718/530/balloon-bunch-sunlight-smile.jpg",
-      title: "First event",
-      shortDescription: "About first event",
-      price: 100,
-      dateTime: "2009-10-14T19:00:00"
-    },
-    {
-      id: 2,
-      title: "Second event",
-      shortDescription: "About second event",
-      price: 0,
-      dateTime: "2009-10-14T19:00:00"
-    },
-    {
-      id: 3,
-      imgSrc:
-        "https://cdn.pixabay.com/photo/2017/12/08/11/53/event-party-3005668_960_720.jpg",
-      title: "Eliot Black",
-      shortDescription:
-        "Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.",
-      price: 0,
-      dateTime: "2009-10-16T19:00:00"
-    },
-    {
-      id: 4,
-      title: "Cat Black",
-      shortDescription:
-        "Cat is a sound engineer living in Nashville who enjoys",
-      price: 0,
-      dateTime: "2009-10-17T19:00:00"
-    },
-    {
-      id: 5,
-      title: "Cat Black",
-      shortDescription:
-        "Matthew is a musician living in Nashville.Cat is a sound engineer living in Nashville who enjoys. ",
-      price: 150,
-      dateTime: "2009-10-18T19:15:00"
-    }
-  ]
+  eventsList: {}
 };
 
 EventsList.propTypes = {
