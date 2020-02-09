b48a855197080fb6d016a153ab8ae0ba8870d13b
Angy <indidel@mail.ru>
2020-02-09 23:07:50 +0100

    Add AppError.
    AppError contains codes of application errors (server side errors). Erro code is used on client to get user friendly error message.

---
+++ events/Models/AppError/AppError.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Events.Models.AppError
+{
+    public enum AppError
+    {
+        None = 0,
+
+        DbRecordNotFound = 20,
+
+        DbRecordAlreadyExists = 21
+
+    }
+}
