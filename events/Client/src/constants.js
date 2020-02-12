4dfa8a0b5e8b2e9097c46bd887344d6ec3354c1b
Angy <indidel@mail.ru>
2020-02-12 19:06:03 +0100

    Add extra languages to SEMANTIC_UI_FLAGS.

--- events/Client/src/constants.js
+++ events/Client/src/constants.js
@@ -6,7 +6,13 @@ export const VALUTA = "NOK";
 export const DEFAULT_LANG = "en";
 
 /* See list of flag names: https://react.semantic-ui.com/elements/flag/ */
-export const SEMANTIC_UI_FLAGS = { en: "america", nb: "norway", ru: "russia" };
+export const SEMANTIC_UI_FLAGS = {
+  en: "america",
+  nb: "norway",
+  ru: "russia",
+  no: "norway",
+  es: "spain"
+};
 
 /* Icons from https://react.semantic-ui.com. They uses font awesome icons */
 export const ICON_NAMES = { pencil: "pencil", add: "add" };
