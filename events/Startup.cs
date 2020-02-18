9876cd14bb198547622cd04f5c24704c07fc5b5a
Angy <indidel@mail.ru>
2020-02-18 19:36:10 +0100

    Change route to  global exception handler in Startup.cs.

--- events/Startup.cs
+++ events/Startup.cs
@@ -53,11 +53,11 @@ namespace Events
 
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseExceptionHandler("/Home/HandleUnhandledExceptions");
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Home/HandleUnhandledExceptions");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
